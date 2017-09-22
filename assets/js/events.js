c1895a7375dbd66b14d3d6ad53aa193501ee4505
AkshaySharma-Akay <akshaysharmaak2@gmail.com>
2017-09-22 21:28:16 +0530

    Added the basic version of home page to be modified

--- assets/js/events.js
+++ assets/js/events.js
@@ -2,19 +2,48 @@ var events = $('.show-events');
 var event_list = $('.event-list');
 var bg = $("#page-bg img");
 var animationend = "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend";
+bg.removeClass('animated bounceInTop');
 
 events.click(function(){
 	if( event_list.is(':visible')){
-		event_list.addClass('animated bounceInLeft')
-		event_list.hide();
+		event_list.addClass('animated bounceOutLeft').one(animationend, function(){
+			event_list.removeClass('animated bounceOutLeft');
+			event_list.hide();
+		});
+		bg.show();
+		bg.addClass('animated bounceInRight').one(animationend,function(){
+			bg.removeClass('animated bounceInRight');
+		});
 	}
 	else{
 		event_list.show();
-		event_list.addClass('animated bounceInLeft');
-		//bg.addClass('animated bounceInLeft');
-		event_list.one(animationend, function(){
-			event_list.removeClass('animated bounceInLeft');
-			//bg.removeClass('animated bounceInLeft');
+		event_list.addClass('animated bounceInLeft').one(animationend, function(){
+			event_list.show();
+		});
+		bg.addClass('animated bounceOutRight').one(animationend,function(){
+			bg.removeClass('animationend bounceOutRight');
+			bg.hide();
+		});
+	}
+});
+
+
+var home = $('.show-home');
+
+home.click(function(){
+	if( event_list.is(':visible')){
+		event_list.addClass('animated bounceOutLeft').one(animationend, function(){
+			event_list.removeClass('animated bounceOutLeft');
+			event_list.hide();
+		});
+		bg.show();
+		bg.addClass('animated bounceInRight').one(animationend,function(){
+			bg.removeClass('animated bounceInRight');
+		});
+	}
+	else{
+		bg.addClass('animated tada').one(animationend, function(){
+			bg.removeClass('animation tada');
 		});
 	}
 });
