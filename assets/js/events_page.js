55ed6d758c46f88a29907a580ab2568d7fd492c5
AkshaySharma-Akay <akshaysharmaak2@gmail.com>
2017-09-25 05:01:23 +0530

    Events page mid section about to complete

--- assets/js/events_page.js
+++ assets/js/events_page.js
@@ -1,11 +1,13 @@
-var dis = $('ev-description')
-var img = $('ev-img');
-var head = $('ev-head')
-var show = $('show-event');
+
 var event = $('.code');/*by default show codexter */
-var clk = $('.b');
+var clk_event = $('.b');
+var clk_ec = $('.event-control');
+var ec = $('.ev-img');
 var animationend = "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend";
-clk.click(function(){
+//var ec_default = $('.ec-default');
+
+clk_event.click(function(){
+//	ec_default.trigger('click');
 	new_event = $(this).attr('event-code');
 	new_event = $('.'+new_event);//important don't remove it
 	event.addClass('animated bounceOutDown').one(animationend, function(){
@@ -16,6 +18,22 @@ clk.click(function(){
 		event.show();
 		event.addClass('animated bounceInDown').one(animationend, function(){
 			event.removeClass('animated bounceInDown');
+		});
+	});
+});
+
+clk_ec.click(function(){
+
+	new_ec = $(this).attr('part-code');
+	new_ec = $('.'+new_ec);//important don't remove it
+	ec.addClass('animated bounceOutLeft').one(animationend, function(){
+		ec.removeClass('animated bounceOut');
+		ec.hide();
+	//	alert('e = '+event+',e2 = ',new_event);
+		ec = new_ec;
+		ec.show();
+		ec.addClass('animated bounceInRight').one(animationend, function(){
+			ec.removeClass('animated bounceInRight');
 		});
 	});
 });
