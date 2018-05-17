c6160b548338641af54ec3d63388baedd6e6b7c6
AkshaySharma-Akay <akshaysharmaak2@gmail.com>
2018-05-18 04:32:09 +0530

    Showing the static data on the template using *ngFor(from a list)

---
+++ angular-tour-of-heros/src/app/mock-heros.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+	{ id : 11, name : 'Mr. Nice'},
+	{ id : 12, name : 'Iron Man'},
+	{ id : 13, name : "The Flash" },
+	{ id : 14, name : "Spider Man" },
+	{ id : 15, name : "The Arrow" },
+	{ id : 16, name : "Dr. Strange" },
+	{ id: 17, name: 'Dynama' },
+  	{ id: 18, name: 'Dr IQ' },
+  	{ id: 19, name: 'Magma' },
+  	{ id: 20, name: 'Tornado' }
+];
